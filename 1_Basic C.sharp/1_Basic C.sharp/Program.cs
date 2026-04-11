aee9e219b05e8171639c3731ec5e2c9f3f427905
Alessio Martini <alemarti.2001@gmail.com>
2026-04-11 22:04:46 +0200

    upload

---
+++ 1_Basic C.sharp/1_Basic C.sharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Basic_C.sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello = "Hello World";
+            int x = 0;
+            Console.WriteLine(hello);
+            string a = Console.ReadLine();
+            Console.WriteLine(a);
+            Console.ReadKey();
+            while(x!=100)
+            {
+                
+                x += 2;
+                if(x>20 && x < 60)
+                {
+                    Console.WriteLine(x);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2_Fattoriale di un Numero INPUT.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int c = 1;
+            int r = 1;
+            Console.WriteLine("Dimmi un numero e ti calcolero il fattoriale");
+            n = Convert.ToInt32(Console.ReadLine());
+            n += 1;
+            for(;c < n; c++)
+            {
+                r *= c;
+            }
+            n -= 1;
+            Console.WriteLine("Il fattoriale di {0} e: {1}", n, r);
+            Console.ReadKey();
+        }
+    }
+}
